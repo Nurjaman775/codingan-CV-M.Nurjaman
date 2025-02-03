e8734235a26d9747ed5bf5590b164ec94ed9b34e
Nurjaman775 <muhamadnurjaman50@gmail.com>
2025-02-03 15:17:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// This script can be used for adding any future interactivity if needed.
+// Right now, it's empty as no interactive functionality is required.
+console.log("CV Online by Muhammad Nurjaman");
